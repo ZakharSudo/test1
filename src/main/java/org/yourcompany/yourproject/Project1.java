2a11047969ad1389a607079aa489887b507b878a
ZakharSudo <arifbaevz@gmail.com>
2025-11-05 00:26:49 +0300

    second commit

--- src/main/java/org/yourcompany/yourproject/Project1.java
+++ src/main/java/org/yourcompany/yourproject/Project1.java
@@ -11,6 +11,6 @@ package org.yourcompany.yourproject;
 public class Project1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("you form a line to formalize the former lies");
     }
 }
